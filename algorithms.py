fee8012aa3207d1a89fc4eabe11839a2e0ef6abf
Nikhil Talagadivi <talagadivinikhil@gmail.com>
2026-05-29 18:04:18 +1000

    implemented DFS

--- algorithms.py
+++ algorithms.py
@@ -1,14 +1,30 @@
 from collections import deque
 
 def bfs(graph, start):
-    # create an empty queue
     q = deque([start])
 
-    # create an empty set visited
     visited = set()
 
     while q:
         v = q.popleft()
+
+        if v in visited:
+            continue
+        visited.add(v)
+
+        yield v, visited.copy()
+
+        for e in graph.adj[v]:
+            if e not in visited:
+                q.append(e)
+
+def dfs(graph, start):
+    q = deque([start])
+
+    visited = set()
+
+    while q:
+        v = q.pop()
 
         if v in visited:
             continue
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 import numpy as np
-from algorithms import bfs
+from algorithms import bfs, dfs
 
 # ----------------------
 # Build graph from input
@@ -20,29 +20,49 @@ while True:
     G.add_edge(a, b)
 
 start = input("Starting node: ")
+traversal_method = input("Type of traversal: (b for BFS or d for DFS): ").strip().lower()
 
 # -----------------------------------------------------------------------------
 # Spring layout for stable animation, seed = 42 since it is the meaning of life
 # -----------------------------------------------------------------------------
 pos = nx.spring_layout(G, seed=42)
 
+if traversal_method == "b":
+    # Start BFS
+    plt.ion()
 
-# Start BFS
-plt.ion()
+    for node, visited in bfs(G, start):
+        plt.clf()
 
-for node, visited in bfs(G, start):
-    plt.clf()
+        node_colors = [
+            "lightgreen" if n in visited else "lightgray"
+            for n in G.nodes()
+        ]
 
-    node_colors = [
-        "lightgreen" if n in visited else "lightgray"
-        for n in G.nodes()
-    ]
+        # draw graph
+        nx.draw(G, pos, with_labels=True, node_color=node_colors)
+        nx.draw_networkx_nodes(G, pos, nodelist=[node], node_color="red")
 
-    # draw graph
-    nx.draw(G, pos, with_labels=True, node_color=node_colors)
-    nx.draw_networkx_nodes(G, pos, nodelist=[node], node_color="red")
+        plt.pause(0.8)
 
-    plt.pause(0.8)
+    plt.ioff()
+    plt.show()
 
-plt.ioff()
-plt.show()
+elif traversal_method == "d":
+    plt.ion()
+
+    for node, visited in dfs(G, start):
+        plt.clf()
+
+        node_colors = [
+            "lightgreen" if n in visited else "lightgray"
+            for n in G.nodes()
+        ]
+
+        nx.draw(G, pos, with_labels=True, node_color = node_colors)
+        nx.draw_networkx_nodes(G, pos, nodelist=[node], node_color="red")
+
+        plt.pause(0.8)
+
+    plt.ioff()
+    plt.show()
